98dff5bb8873f0a32e54254ed29da41b46f50a3e
Kadir Alpay <kadir.alpay08@gmail.com>
2026-06-29 14:51:46 -0400

    Arduino sketch prints raw angle,distance over serial (hopefully this time the files upload

---
+++ RadarApp/mainwindow.cpp
@@ -0,0 +1,36 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <QSerialPortInfo>
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    serial = new QSerialPort(this);
+    serial->setPortName("COM5"); // your actual port
+    serial->setBaudRate(QSerialPort::Baud9600);
+
+    if (!serial->open(QIODevice::ReadOnly)) {
+        qDebug() << "Failed to open port:" << serial->errorString();
+    }
+
+    connect(serial, &QSerialPort::readyRead, this, [=]() {
+        while (serial->canReadLine()) {
+            QByteArray line = serial->readLine().trimmed();
+            QList<QByteArray> parts = line.split(',');
+            if (parts.size() == 2) {
+                int angle = parts[0].toInt();
+                int distance = parts[1].toInt();
+                qDebug() << "angle:" << angle << "distance:" << distance;
+            }
+        }
+    });
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
